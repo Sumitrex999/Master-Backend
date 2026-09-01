8bd9a3922ebb8d11734a836ba9bd19ed27ddd71a
Sumit Rex <sumitrex666@gmail.com>
2026-09-01 22:39:31 +0530

    Backend CRUD

---
+++ Backend-express/notes.md
@@ -0,0 +1,12 @@
+#Postman (isClient : like Browser) - to send req to the server.
+req.params - here params is the dynamic value of the req API.
+
+#Methods:
+Delete - params: delete by id.
+put- update the entire data or created a new data(complete update).
+patch - update the existing data (partial update).
+
+#Status Code 
+204 - delete req success, does n't display the message or response in postman.
+201 - create req success.
+200 - any req success.
--- express-server/server.js
+++ express-server/server.js
@@ -3,10 +3,62 @@ import express from "express";
 // To run the instance of express
 const app = express();
 
+// middleware in express so that the express can read the incoming data(by default express can't read the incoming data , otherwise req.body will always be undefined)
+app.use(express.json())
+
+let notes = [];
+
+
 // program the server to send the response (if req comes to "/" - endpoint);
 app.get("/", function(req, res){
-    return res.send("hello KODR")
+    return res.json({
+        message:"API is alive", //used for success or fail infomation
+        data:"hello Kodr"
+    })
 });
+
+
+
+app.post("/api/notes", function(req, res){
+
+    const note = {
+        title: req.body.title,
+        description: req.body.description
+    }
+
+    notes.push(note)
+
+     res.status(201).json({
+        message:"Note created Successfully", //used for success or fail infomation
+        note
+    })
+})
+
+app.get("/api/notes", function(req,res){
+    res.status(200).json({
+        message:"Note fetched Successfully", //used for success or fail infomation
+        notes
+    })
+})
+
+app.delete("/api/notes/:id", function(req,res){
+    const id = Number(req.params.id);
+    notes = notes.filter((_,index)=> index !== id);
+    res.sendStatus(204);
+
+})
+
+app.patch("/api/notes/:id", function(req,res){
+    let {title, description} = req.body;
+    const id = Number(req.params.id);
+    notes = notes.map((note,index)=> index === id? {...note, title, description }:note);
+     res.status(200).json({
+        message: "Note updated successfully",
+        notes
+    });
+});
+
+
 
 app.get("/about", function(req, res){
     return res.send("About KODR")
